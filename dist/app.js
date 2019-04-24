f636cdb698142f04d43ca5e4ea8046ed226500f9
alexm200 <alexm200@hotmail.com>
2019-04-24 17:12:49 +0300

    commit

--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000;
 var app = express();
 app.get("/", function (req, res) {
   res.send(JSON.stringify({
-    Hello: "World"
+    Hello: port
   }));
 });
 app.listen(port, function () {
